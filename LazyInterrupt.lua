16790458a75cbd2ae7a006bbb6a0daa7ddd3411a
Thijs ter Velde <thijsiee.tv@gmail.com>
2015-07-23 02:41:35 +0200

    non-laggy rewrite

--- LazyInterrupt.lua
+++ LazyInterrupt.lua
@@ -1,72 +1,82 @@
-local f = CreateFrame("Frame")
-local f2 = CreateFrame("Frame")
-f2:RegisterEvent("PLAYER_ENTERING_WORLD")
-f2:RegisterEvent("GROUP_ROSTER_UPDATE")
-local bloodlust = {
-	[80353] = true,
-	[90355] = true,
-	[160452] = true,
-	[32182] = true,
-	[2825] = true,
-	[178207] = true,
-}
-local buffs = {
-	[175215] = true, --Savage Feast
-	[173979] = true, --Feast of the waters
-	[173978] = true, --Feast of Blood
-	[185705] = true, --Fancy Darkmoon Feast
-	[185708] = true, --Sugar-Crusted Fish Feast
-	[29893] = true, --Soulwell
-	[698] = true, --Summoning Portal
-	[174889] = true, --Aeda Perk: Ritual of Summoning
-	[174887] = true, --Illona Perk: Guiding Light
-}
-local channel
-local prevMsg
-local prevTime = 0
+local frame = CreateFrame("Frame")
+local cFrame = CreateFrame("Frame")
 
-local function getChannel()
+local bloodlust = {[80353]=true,[90355]=true,[160452]=true,[32182]=true,[2825]=true,[178207]=true}
+local buffs = {[175215]=true,[173979]=true,[173978]=true,[185705]=true,[185708]=true,[29893]=true,[698]=true,[174889]=true,[174887]=true}
+local channel, prevMsg, prevTime, _playerGUID, _petGUID
+
+frame:SetScript('OnEvent', function(self, event, ...)
+	self[event](...)
+	end)
+cFrame:SetScript('OnEvent', function(self,_,_,event,...)
+	if self[event] then
+		self[event](self, ...)
+	end
+end)
+
+frame:RegisterEvent("PLAYER_ENTERING_WORLD")
+frame:RegisterEvent("PLAYER_REGEN_ENABLED")
+frame:RegisterEvent("PLAYER_REGEN_DISABLED")
+frame:RegisterEvent("GROUP_ROSTER_UPDATE")
+
+local function setChannel()
 	if IsInGroup(LE_PARTY_CATEGORY_INSTANCE) then
-		return "INSTANCE_CHAT"
+		channel = "INSTANCE_CHAT"
 	elseif IsInRaid() then
-		return "RAID"
+		channel = "RAID"
 	elseif IsInGroup() then
-		return "PARTY"
+		channel = "PARTY"
 	else
-		return false
+		-- Debug, set return to true.
+		channel = true
 	end
 end
 
 local function announce(msg)
-	if prevMsg ~= msg or (GetTime() - prevTime > 10 ) then
+	if (prevMsg ~= msg or (GetTime() - prevTime > 10 )) and channel then
 		prevMsg = msg
 		prevTime = GetTime()
-	SendChatMessage(msg,channel,"COMMON")
+		SendChatMessage(msg,channel,"COMMON")
 	end
 end
 
-f:SetScript("OnEvent", function(self, event, ...)
-	local _,eventType,_,source,sourceName,_,_,_,npcName,_,_,buffID,buffName,_,spellID= ...
-	if eventType == "SPELL_INTERRUPT" and (source == UnitGUID("player") or source == UnitGUID("pet")) then
-		local msg = ACTION_SPELL_INTERRUPT:gsub("^%l", string.upper).." "..npcName..": "..GetSpellLink(spellID)
-		announce(msg)
-	elseif eventType == "SPELL_AURA_APPLIED" and bloodlust[buffID] then
-		local msg = sourceName.." casted "..GetSpellLink(buffID).."!"
-		announce(msg)
-	elseif eventType == "SPELL_CREATE" and buffs[buffID] then
-		local msg = sourceName.." placed down "..npcName
-		announce(msg)
+function registerEvents(register)
+	setChannel()
+	print("channel is",channel)
+	if register == false or channel == false then
+		print("UnregisterEvent1")
+		cFrame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+	elseif channel or register then
+		cFrame:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+		print("RegisterEvent")
+	else
+		cFrame:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
+		print("UnregisterEvent2")
 	end
+end
 
-end)
-
-f2:SetScript("OnEvent", function(self,event,...)
-	if event == "PLAYER_ENTERING_WORLD" or event == "GROUP_ROSTER_UPDATE" then
-		channel = getChannel()
-		if channel then
-			f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-		else
-			f:UnregisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
-		end
+function cFrame:SPELL_SUMMON(_,playerGUID)
+	if playerGUID == _playerGUID then
+		_petGUID = UnitGUID("pet")
 	end
-end)
+end
+
+function frame:PLAYER_ENTERING_WORLD()
+	_playerGUID = UnitGUID("player")
+	_petGUID = UnitGUID("pet")
+	registerEvents()
+end
+
+function frame:GROUP_ROSTER_UPDATE()
+	registerEvents()
+end
+
+function frame:PLAYER_REGEN_ENABLED()
+	registerEvents()
+end
+
+function frame:PLAYER_REGEN_DISABLED()
+	registerEvents(false)
+end
+
+
